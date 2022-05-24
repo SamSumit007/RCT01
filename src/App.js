b2fa4e1f85fa044c9701b241d357f3ec8fa7c2db
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-05-24 16:55:58 +0530

    day2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
-import Counter from './components/Counter';
-import Todo from './components/Todo';
+import Hello from './components/hello';
+import Answer from './components/Answers';
 
 function App() {
+let info = [
+  {name: "Sumit", place:"India"},
+  {name: "Sam", place:"Aus"},
+  {name: "Thakur", place:"US"},
+];
+
+
   return (
     <div className="App">
-<Counter  initialValue={1000} /> 
-<Todo />
-  </div>
+      
+        
+        <Hello /> 
+       
+        
+    <h1>New: 1</h1>
+    {info.map((user) => (
+      <div>
+        <Answer {...user}  />
+      </div>
+    ))}
+
+    </div>
   );
 }
 
 export default App;
+
