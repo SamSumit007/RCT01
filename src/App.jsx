0f7d11804ec708bb2640cb40b20785fecaccc6aa
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-05-31 18:22:26 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,19 @@
-import { useContext, useReducer } from 'react';
 import './App.css';
-import Body from './Components/Body';
-import Navbar from './Components/Navbar';
-import { ThemeContext } from './context/ThemeContext';
+import Todos, { Todo } from './components/Todos';
 
-
-const reducer = (counter, action)=> {
-  switch (action.type) {
-    case 'increment':{
-      return counter + 1;}
-    case 'decrement':{
-      return counter - 1;}
-    default:{
-      return counter
-    }
-  }
-}
 function App() {
-  const [counter, dispatch] = useReducer(reducer, 0);
-const {isLight} = useContext(ThemeContext);
+
+
 
   return (
-    <div className={`App ${isLight ?"Light" : "Dark" }`}>
-      <div>
-      Counter: {counter}
-      <div>
-        <button onClick={() => dispatch({type:'increment' })}>+</button>
-        <button onClick={() => dispatch({type:'decrement' })}>-</button>
-      </div>
-      </div>
-      <div>
+    <div className="App">
+      <Todos />
+      {/* <Todo /> */}
         
-      <Navbar />
-    <Body />
-      </div>
+       
     </div>
   );
 }
 
 export default App;
+
--- src/context/CartContext.jsx
+++
@@ -1,5 +0,0 @@
-import React, { createContext } from "react";
-export const CartContext = createContext();
-export const CartProvider =({children})  =>{
-    return <CartContext.Provider value={100}>{children}</CartContext.Provider>
-} 
