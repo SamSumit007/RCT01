b2ce17ec5f7cf9c516b9a7541267d20644a01469
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-06-07 16:11:49 +0530

    Day13

--- Redux/src/index.js
+++ Redux/src/index.js
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/Auth';
-
+import { Provider} from 'react-redux';
+import { store } from './Store/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <AuthProvider>
+   <Provider store={store}>
     <App />
-    </AuthProvider>
-    </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
